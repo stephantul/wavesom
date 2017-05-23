Normalization, fixed to pip package

wavesom/experiments/mental_lexicon.py
@@ -1,16 +1,14 @@
 import json
-import numpy as np
 import time
-
-from somplay.amlap.lexisom import Lexisom
-from somplay.experiments.preprocessing.ortho import Orthographizer
-from somplay.experiments.preprocessing.sivi.ipapy_features import ipapy_sivi
-from somplay.experiments.preprocessing.sampler import random_sample
-from somplay.experiments.preprocessing.bow_encoder import BowEncoder
-from somplay.experiments.preprocessing.sivi.binary_sivi import binary_sivi
-
-from collections import Counter, defaultdict
+from collections import defaultdict
 from string import ascii_lowercase
+
+import numpy as np
+from wavesom.experiments.preprocessing.sivi.ipapy_features import ipapy_sivi
+
+from wavesom.experiments.preprocessing.bow_encoder import BowEncoder
+from wavesom.experiments.preprocessing.sampler import random_sample
+from wavesom.wavesom import Wavesom
 
 
 def check(sample_set, s, o):
@@ -42,7 +40,7 @@ if __name__ == "__main__":
 
     max_len = 6
 
-    sivi = binary_sivi(4)
+    sivi = ipapy_sivi(4)
     o = BowEncoder()
     orth_vec_len = max_len * o.datalen
     np.random.seed(44)
@@ -96,7 +94,7 @@ if __name__ == "__main__":
 
     start = time.time()
 
-    s = Lexisom((20, 20), X.shape[1], 1.0, orth_len=orth_vec_len, phon_len=X.shape[1] - orth_vec_len)
+    s = Wavesom((20, 20), X.shape[1], 1.0, orth_len=orth_vec_len, phon_len=X.shape[1] - orth_vec_len)
     s.train(X, 100, total_updates=10000, batch_size=100, show_progressbar=True, stop_nb_updates=0.5)
 
     # TODO: increase map size, play with hyperparams

wavesom/visualization/simple_viz.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 from pylab import rcParams
-rcParams['figure.figsize'] = 10, 10
+rcParams['figure.figsize'] = 8, 8
 
 v = np.array([[0.267004,  0.004874,  0.329415],
               [0.270595,  0.214069,  0.507052],
@@ -57,11 +57,7 @@ def show_label_activation_map(labels, width, activation, interpolation='sinc', c
     if cmap is None:
         cmap = l
 
-    a = np.zeros((width, width))
-    for idx, x in enumerate(activation):
-        a[idx % width, idx // width] = x
-
-    plt.imshow(a, extent=[0, width, width, 0], interpolation=interpolation, cmap=cmap)
+    plt.imshow(activation, extent=[0, width, width, 0], interpolation=interpolation, cmap=cmap)
 
 
 def show_label_scatter_map(labels, width, arrow_vectors):

wavesom/wavesom.py
@@ -32,9 +32,9 @@ class Wavesom(Batch_Som):
     @classmethod
     def load(cls, path):
         """
-        Loads a Lexisom
+        Loads a Wavesom
 
-        :param path: The path to the JSON file where the lexisom is stored
+        :param path: The path to the JSON file where the wavesom is stored
         :return: A lexisom.
         """
         data = json.load(open(path))
@@ -59,7 +59,7 @@ class Wavesom(Batch_Som):
 
     def save(self, path):
         """
-        Save a lexisom
+        Save a wavesom
 
         :param path: The path to save the lexisom to.
         :return: None
@@ -98,7 +98,7 @@ class Wavesom(Batch_Som):
 
         for x in X:
 
-            distance = self._euclidean(x, weights=temp_weights)
+            distance = self.distance_function(x, weights=temp_weights)
             distances.extend(distance)
 
         return np.array(distances)
@@ -148,11 +148,31 @@ class Wavesom(Batch_Som):
         return {k: np.array(v) for k, v in dicto.items()}
 
     def propagate_values(self, vector, max_depth=5, num=3, start_idx=0, end_idx=None):
+        """
+
+
+        :param vector:
+        :param max_depth:
+        :param num:
+        :param start_idx:
+        :param end_idx:
+        :return:
+        """
 
         p = self.propagate(vector, max_depth, num, start_idx, end_idx, False)
         return {k: v.mean(axis=0) for k, v in p.items()}
 
     def get_value(self, vector, max_depth=5, num=3, start_idx=0, end_idx=None):
+        """
+
+
+        :param vector:
+        :param max_depth:
+        :param num:
+        :param start_idx:
+        :param end_idx:
+        :return:
+        """
 
         s = self.propagate_values(vector, max_depth, num, start_idx, end_idx)
         return np.array(list(s.values())).sum(axis=0)