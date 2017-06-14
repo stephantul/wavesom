1883f1f8fcb5a162c62ec7b2c53b51efb616935b
stephantul <stephan.tulkens@uantwerpen.be>
2017-06-14 19:42:46 +0200

    Amlap experiments

--- wavesom/experiments/mental_lexicon_bi.py
+++ wavesom/experiments/mental_lexicon_bi.py
@@ -1,32 +1,39 @@
 import json
 import numpy as np
-import time
 
 from wavesom.wavesom import Wavesom
-from wavesom.experiments.preprocessing.ortho import Orthographizer
-from wavesom.experiments.preprocessing.sivi.ipapy_features import ipapy_sivi
-from wavesom.experiments.preprocessing.sampler import random_sample
+from wavesom.experiments.setup import setup
+from wavesom.experiments.preprocessing.orthographizer.mcclelland import mcclelland_orthography
 
-from collections import Counter, defaultdict
-from string import ascii_lowercase
+from collections import defaultdict
 
 
-def show(word, o, orth_vec_len, s, sap, depth=5, num=3):
+def show(stimulus, orthographizer, wavesom, sap, depth=5, num=3):
+    """
+    Shows the response of the map to some stimulus
 
-    from wavesom.visualization.simple_viz import show_label_arrow_map, show_label_scatter_map, show_label_activation_map
+    :param stimulus: The input stimulus as a string.
+    :param orthographizer: The orthographizer used to encode the input stimulus.
+    :param wavesom: The trained wavesom.
+    :param sap: The word2idx dictionary, for visualization.
+    :param depth: The depth to descend in the tree.
+    :param num: The number of neighbors to consider in the computation.
+    :return: None
+    """
+    # Imports
+    from wavesom.visualization.simple_viz import show_label_activation_map
     import matplotlib.pyplot as plt
 
+    # Close previous plot
     plt.close()
 
-    a = transfortho(word, o, orth_vec_len)
-    b = s.propagate(a, max_depth=depth, num=num)
-
-    # show_label_arrow_map(sap, 10, b)
-    # show_label_scatter_map(sap, 10, b)
-    show_label_activation_map(sap, 10, s.get_value(a, max_depth=depth, num=num).reshape(10, 10).T)
+    # Transform the stimulus to a vector.
+    a = transfortho(stimulus, orthographizer, wavesom.orth_len)
+    # Show the map's response to the stimulus
+    show_label_activation_map(sap, 10, wavesom.activate_state(a, max_depth=depth, num=num).reshape(10, 10).T)
 
 
-def evaluate(pas_dict, s):
+def evaluate(pas_dict, s, o):
 
     results = []
     words = []
@@ -50,139 +57,59 @@ def dist_to_lexicon(vec, lexicon, X, num_to_return=5):
 def transfortho(x, o, length):
 
     zero = np.zeros((length,))
-    vec = o.transform(x).ravel()
+    vec = o.vectorize(x).ravel()
     zero[:len(vec)] += vec
 
     return zero
 
 if __name__ == "__main__":
 
-    english_dict = json.load(open("data/english.json"))
-    dutch_dict = json.load(open("data/dutch.json"))
+    path = ""
+    dicto = json.load(open("data/syllable_lexicon.json"))
 
     max_len = 7
 
-    eng_id = {"{0} {1}".format(o, p) for o, _, p in english_dict}
-    dutch_id = {"{0} {1}".format(o, p) for o, _, p in dutch_dict}
+    wordlist = {'kind', 'mind', 'bind',
+                'room', 'wolf', 'way',
+                'wee', 'eten', 'eating',
+                'wind', 'lead', 'speed',
+                'meat', 'meet', 'meten',
+                'spring', 'winter', 'spel',
+                'speel', 'spill', 'spoon',
+                'moon', 'boon', 'doen',
+                'biet', 'beat', 'spijt',
+                'tijd', 'mijt', 'klein',
+                'fijn', 'rein', 'reign',
+                'feign', 'fine', 'mine',
+                'dine', 'wine', 'wijn',
+                'weinig', 'speel', 'meel',
+                'keel', 'veel', 'kiel',
+                'wiel', 'wheel', 'feel',
+                'deal', 'spool', 'spoel',
+                'trol', 'troll', 'wolf',
+                'rond', 'mond', 'mound',
+                'hound', 'found', 'find',
+                'lined', 'spine', 'mine',
+                'cake', 'keek', 'leem',
+                'dessert', 'desert', 'model',
+                'curve', 'dies', 'nies', 'vies',
+                'boot', 'potent', 'sate',
+                'tree', 'rapport', 'lied',
+                'bied', 'mond', 'kei',
+                'steen', 'meen', 'geen',
+                'keek', 'leek', 'gek',
+                'creek', 'ziek', 'piek'}
 
-    lang_dict = {}
+    o = mcclelland_orthography()
 
-    for e in eng_id.union(dutch_id):
-        if e in eng_id and e in dutch_id:
-            lang_dict[e] = 'both'
-        elif e in eng_id:
-            lang_dict[e] = 'eng'
-        else:
-            lang_dict[e] = 'dut'
+    X, X_orig, s_, c = setup(dicto, max_len, wordlist)
+    orth_vec_len = 14 * max_len
 
-    sivi = ipapy_sivi(3)
-    o = Orthographizer()
-    orth_vec_len = max_len * o.datalen
+    s = Wavesom.load(path, np)
 
-    np.random.seed(44)
+    w2l = defaultdict(list)
+    l2w = defaultdict(list)
 
-    listo = []
-    X = []
-
-    idx = 0
-
-    w2id = defaultdict(list)
-
-    frequencies = []
-
-    words = {'kind', 'mind', 'bind',
-             'room', 'wolf', 'way',
-             'wee', 'eten', 'eating',
-             'wind', 'lead', 'speed',
-             'meat', 'meet', 'meten',
-             'spring', 'winter', 'spel',
-             'speel', 'spill', 'spoon',
-             'moon', 'boon', 'doen',
-             'biet', 'beat', 'spijt',
-             'tijd', 'mijt', 'klein',
-             'fijn', 'rein', 'reign',
-             'feign', 'fine', 'mine',
-             'dine', 'wine', 'wijn',
-             'weinig', 'speel', 'meel',
-             'keel', 'veel', 'kiel',
-             'wiel', 'wheel', 'feel',
-             'deal', 'spool', 'spoel',
-             'trol', 'troll', 'wolf',
-             'rond', 'mond', 'mound',
-             'hound', 'found', 'find',
-             'lined', 'spine', 'mine',
-             'cake', 'keek', 'leem',
-             'dessert', 'desert', 'model',
-             'curve', 'dies', 'nies', 'vies',
-             'boot', 'potent', 'sate',
-             'tree', 'rapport', 'lied',
-             'bied', 'mond', 'kei',
-             'steen', 'meen', 'geen',
-             'keek', 'leek', 'gek',
-             'creek', 'ziek', 'piek'}
-
-    english_dict.extend(dutch_dict)
-    word_dict = defaultdict(list)
-
-    for ortho, freq, phono in english_dict:
-
-        if ortho not in words:
-            continue
-
-        if len(set(ortho) - set(ascii_lowercase)) > 0:
-            continue
-
-        if len(ortho) > max_len:
-            print("{0} too long: {1}".format(ortho, len(ortho)))
-            continue
-
-        orth = np.zeros((orth_vec_len,))
-        x_orth = o.transform(ortho).ravel()
-
-        orth[:x_orth.shape[0]] = x_orth
-
-        try:
-            x_phon = sivi.vectorize_single(phono, left=False)
-        except ValueError as e:
-            print("Skipped {0}".format(ortho, phono))
-            print(e)
-            continue
-
-        listo.append(" ".join((ortho, phono)))
-        X.append(np.hstack([orth, x_phon]))
-
-        w2id[ortho].append(idx)
-        word_dict[ortho].append(phono)
-
-        frequencies.append(freq)
-        idx += 1
-
-    X_orig = np.array(X)
-    frequencies = np.array(frequencies) + 1
-
-    sample = np.array(random_sample(listo, frequencies, 10000, min_count=10))
-    s_ = list(set(sample))
-    c_ = Counter(sample)
-
-    X = np.array([X_orig[listo.index(item)] for item in sample])
-    X_orig = np.array([X_orig[listo.index(x)] for x in s_])
-
-    start = time.time()
-
-    # s = Lexisom((20, 20), X.shape[1], 1.0, orth_len=orth_vec_len, phon_len=X.shape[1] - orth_vec_len)
-    # s.train(X, 1000, total_updates=10000, batch_size=100, show_progressbar=True, stop_nb_updates=0.5)
-    # s = Lexisom.load("bi_lexicon.json")
-    # s = Lexisom.load("bi_freq.json")
-    s = Wavesom.load("saved_models/log_freq_1000.json")
-
-    print("Took {0} seconds".format(time.time() - start))
-
-    sap = defaultdict(list)
-    pas = defaultdict(list)
-
-    for k, v in zip(s_, s.predict(X_orig)):
-
-        orth, pron = k.split()
-
-        sap[v].append(k)
-        pas[orth].append(v)
+    for n, x in zip(s_, s.predict(X_orig)):
+        w2l[n].append(x)
+        l2w[x].append(n)
---
+++ wavesom/experiments/train_mental_lexicon.py
@@ -0,0 +1,76 @@
+import json
+import numpy as np
+import time
+import argparse
+
+from wavesom.wavesom import Wavesom
+from wavesom.experiments.setup import setup
+import cupy as cp
+
+from collections import Counter, defaultdict
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpu", type=int, help="The gpu to use", default=None)
+    parser.add_argument("--file", type=str, help="The path to save the trained model to")
+    parser.add_argument("--dim", type=int, help="The map dimensionality", default=30)
+    parser.add_argument("--epochs", type=int, help="The number of epochs to train", default=100)
+    args = parser.parse_args()
+
+    dicto = json.load(open("data/syllable_lexicon.json"))
+
+    max_len = 7
+
+    wordlist = {'kind', 'mind', 'bind',
+                'room', 'wolf', 'way',
+                'wee', 'eten', 'eating',
+                'wind', 'lead', 'speed',
+                'meat', 'meet', 'meten',
+                'spring', 'winter', 'spel',
+                'speel', 'spill', 'spoon',
+                'moon', 'boon', 'doen',
+                'biet', 'beat', 'spijt',
+                'tijd', 'mijt', 'klein',
+                'fijn', 'rein', 'reign',
+                'feign', 'fine', 'mine',
+                'dine', 'wine', 'wijn',
+                'weinig', 'speel', 'meel',
+                'keel', 'veel', 'kiel',
+                'wiel', 'wheel', 'feel',
+                'deal', 'spool', 'spoel',
+                'trol', 'troll', 'wolf',
+                'rond', 'mond', 'mound',
+                'hound', 'found', 'find',
+                'lined', 'spine', 'mine',
+                'cake', 'keek', 'leem',
+                'dessert', 'desert', 'model',
+                'curve', 'dies', 'nies', 'vies',
+                'boot', 'potent', 'sate',
+                'tree', 'rapport', 'lied',
+                'bied', 'mond', 'kei',
+                'steen', 'meen', 'geen',
+                'keek', 'leek', 'gek',
+                'creek', 'ziek', 'piek'}
+
+    X, X_orig, s_, c = setup(dicto, max_len, wordlist)
+    orth_vec_len = 14 * max_len
+
+    start = time.time()
+
+    if args.gpu:
+
+        with cp.cuda.Device(args.gpu):
+            X = cp.asarray(X, cp.float32)
+            X_orig = cp.asarray(X, cp.float32)
+
+            s = Wavesom((args.dim, args.dim), X.shape[1], 1.0, orth_len=orth_vec_len, phon_len=X.shape[1] - orth_vec_len)
+            s.fit(X, 10, total_updates=100, batch_size=250, show_progressbar=True, stop_nb_updates=0.5)
+
+    else:
+
+        s = Wavesom((args.dim, args.dim), X.shape[1], 1.0, orth_len=orth_vec_len, phon_len=X.shape[1] - orth_vec_len)
+        s.fit(X, 10, total_updates=100, batch_size=250, show_progressbar=True, stop_nb_updates=0.5)
+
+    s.save(args.path)
